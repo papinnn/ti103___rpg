a36cbedaf6239c051cdbc768ccf9e024a4f0e0c5
papinnn <papinnn111@gmail.com>
2022-03-19 09:04:16 -0500

    bienvenue sur pavgame

--- ti103___rpg/ti103___rpg.py
+++ ti103___rpg/ti103___rpg.py
@@ -1,1 +1,11 @@
-"""Main module."""
+import pygame
+
+
+pygame.init()
+window = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Pavgame")  # le nom du jeu
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit(0)
