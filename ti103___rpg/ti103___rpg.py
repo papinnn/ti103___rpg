eb631bb3a3fc8b1c742080879a2f21c9787f93a6
papinnn <papinnn111@gmail.com>
2022-03-31 17:59:28 -0500

    le nouveau commite

--- ti103___rpg/ti103___rpg.py
+++ ti103___rpg/ti103___rpg.py
@@ -1,26 +1,52 @@
 import pygame
+"""
+
+>>> len(charge_image())
+12
+"""
+
+
+
+
+HEIGHT = 16
+WIDTH = 24
 
 
 pygame.init()
-window = pygame.display.set_mode((800,600))
-
-pygame.display.set_caption("Psplay.toggle_fullscreen()avgame")  # le nom du jeu
-
-
+window = pygame.display.set_mode((36 * WIDTH, 36 * HEIGHT))
+window.fill((255, 255, 255))
 run = True
-shift = 0
+
+
+import os           # Module os de python - Qui permet a Python de communiquer avec Windows
+                    # Systeme de fichier de Windows est accessible a travers le module 'os'
+print(os.getcwd())  # getcwd = get current working directory, autrement dit, la ou s'execute votre programme
+
+
+
+def charge_image():#decoupe l image en 16 rectangle
+    image = pygame.image.load("ressources/tuiles.png").convert()
+    width, height = image.get_size()
+    tuiles = []
+    for x in range(width//WIDTH):
+      ligne = []
+      for y in range(height//HEIGHT):
+        rect = (x * WIDTH, y * HEIGHT, WIDTH, HEIGHT)
+        ligne.append(image.subsurface(rect))
+      tuiles.append(ligne)
+    return tuiles
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run=False
+            run = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                shift -= 10
-                shift %= 100
+    for x, ligne in enumerate(charge_image()):
+        for y, tuile in enumerate(ligne):
+            tuile = pygame.transform.scale(tuile, (WIDTH * 8, HEIGHT * 8))
+            window.blit(tuile, (x * (WIDTH * 9), y * (HEIGHT * 9)))
 
-    window.fill((100, 255, 255))
-    pygame.draw.circle(window,(255,0,0),(shift,shift),100)
+    pygame.display.flip()
 
-    pygame.display.flip()#jamais oublier pour faire des fenetre en couleur et le remplis de cet couleur
 pygame.quit()
+
